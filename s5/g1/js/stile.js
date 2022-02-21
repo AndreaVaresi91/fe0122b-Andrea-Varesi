b9082582a9e631e6294f3b6a63505a9628f736fe
AndreaVaresi91 <animadelchaos@gmail.com>
2022-02-21 16:36:15 +0100

    pratica con Date

--- s5/g1/js/stile.js
+++ s5/g1/js/stile.js
@@ -8,6 +8,7 @@ document.getElementById("uno").innerHTML=(dataAttuale);
 //mostra elementi singoli della data//
 console.log(dataAttuale.getDate());
 console.log(dataAttuale.getMonth());
+document.getElementById("paragrafo").innerHTML = (dataAttuale.getFullYear()) + " " + (dataAttuale.getDate()) + " " + (dataAttuale.getMonth()+1);
 
 //formattazione in formato europeo//
 
@@ -18,3 +19,23 @@ console.log(event.toLocaleDateString('it-IT', options));
 //formattata in html//
 
 document.getElementById("due").innerHTML=(event.toLocaleDateString('it-IT', options));
+
+// orologio //
+
+setInterval(Orologio,1000);
+  function addZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+  }
+  function Orologio()
+  {
+    var d = new Date();
+    var date = d.getDate();
+    var ora = addZero(d.getHours());
+    var min = addZero(d.getMinutes());
+    var sec = addZero(d.getSeconds());
+    document.getElementById("orologio").innerHTML=ora+":"+min+":"+sec;}
+
+    //fine//
